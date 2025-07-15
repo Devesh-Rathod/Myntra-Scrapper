bb9d7ec46c1a661bfee084c1d9de158ecb9b1414
devesh <deveshrathod15@gmail.com>
2025-07-15 06:10:21 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+setup(
+    name="myntra_scrapper_project",
+    version="0.1",
+    author="Devesh Rathod",
+    author_email="deveshrathod15@gmail.com",
+    description="A project to scrape data from Myntra",
+    packages=find_packages(),
+    install_requires=[
+    ],
+)
